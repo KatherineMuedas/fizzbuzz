9ea1aeeca0610e27cf030db148de5493b8498efd
katherine <muedasnka@gmail.com>
2019-03-18 15:31:34 -0400

    adds test for generate fizz for multiple of 3, and make it pass

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -2,4 +2,12 @@ class FizzBuzz
   def numbers
     (1..100).to_a
   end
+
+  def sequence
+    numbers.collect do |n|
+      if (n % 3 == 0)
+        'Fizz'
+      end
+    end
+  end
 end
--- test_fizz_buzz.rb
+++ test_fizz_buzz.rb
@@ -7,4 +7,10 @@ class TestFizzBuzz < Minitest::Test
     result = fb.numbers
     assert_equal (1..100).to_a, result
   end
+
+  def test_generate_fizz_for_multiple_of_3
+    fb = FizzBuzz.new
+    result = fb.sequence
+    assert_equal 'Fizz', result[2]
+  end
 end
