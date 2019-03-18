1b0e101a90543109b3040bb861f451ba3afa0714
katherine <muedasnka@gmail.com>
2019-03-18 17:02:25 -0400

    moves numbers method to private methods and remove test

--- test_fizz_buzz.rb
+++ test_fizz_buzz.rb
@@ -2,12 +2,6 @@ require 'minitest/autorun'
 require_relative 'fizz_buzz'
 
 class TestFizzBuzz < Minitest::Test
-  def test_generate_number_for_1_to_100
-    fb = FizzBuzz.new
-    result = fb.numbers
-    assert_equal (1..100).to_a, result
-  end
-
   def test_generate_fizz_for_multiple_of_3
     fb = FizzBuzz.new
     result = fb.sequence
