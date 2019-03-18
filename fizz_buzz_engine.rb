6cadf1230886f439fb8cf56d0be29bdbd9df2783
katherine <muedasnka@gmail.com>
2019-03-18 17:10:51 -0400

    adds a LOOKUP HASH to check if number is multiple of a given number and return value fizz, buzz, fizzbuzz

--- fizz_buzz_engine.rb
+++ fizz_buzz_engine.rb
@@ -1,12 +1,13 @@
 class FizzBuzzEngine
+  LOOKUP = {15 => 'FizzBuzz', 3 => 'Fizz', 5 => 'Buzz'}
   def initialize(number)
     @number = number
   end
 
   def value
-    return 'FizzBuzz' if multiple_of(15)
-    return 'Fizz' if multiple_of(3)
-    return 'Buzz' if multiple_of(5)
+    LOOKUP.keys.each do |key|
+      return LOOKUP[key] if multiple_of(key)
+    end
     @number
   end
 
